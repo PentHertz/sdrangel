255e393e9ffa27f50ade996ba6bedd653cb2005c
FlUxIuS <fluxius@gmail.com>
2026-03-03 20:11:04 +0100

    fixing license agreement

--- cmake/ci/build_sdrplay.sh
+++ cmake/ci/build_sdrplay.sh
@@ -5,4 +5,8 @@ mkdir -p sdrplayapi && cd sdrplayapi
 git clone https://github.com/srcejon/sdrplayapi.git
 cd sdrplayapi
 sed -i s/more\ -d/cat/ install_lib.sh
-sudo yes | bash install_lib.sh
+if [ "$(id -u)" -eq 0 ]; then
+  yes | bash install_lib.sh
+else
+  sudo bash -c 'yes | bash install_lib.sh'
+fi
